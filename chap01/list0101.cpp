894aae9fcfb4963dff30820a5bcdcc3708c7cc8c
rkarsnk <git-repos-rkarsnk@rkarsnk.jp>
2022-08-01 15:33:04 +0900

    1st commit.

---
+++ chap01/list0101.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "初めてのC++プログラム。\n";
+    cout << "画面に出力しています。\n";
+}
---
+++ chap01/list0102.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "初めてのC++プログラム。\n画面に出力しています。\n";
+}
